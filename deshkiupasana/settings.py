b4a141d0bd073df2cb16663626100a6018f60d62
abhishek <kumar008abhishek>
2020-06-06 10:45:19 +0530

    settings changes

--- deshkiupasana/settings.py
+++ deshkiupasana/settings.py
@@ -18,9 +18,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from news.secret import secret
 SECRET_KEY = secret
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deshkiupasana.herokuapp.com']
 
 
 # Application definition

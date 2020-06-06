15ca9e3c50793f3d73ed44bff6ffaadad9f4ac36
abhishek <kumar008abhishek>
2020-06-06 23:06:11 +0530

    static changes

--- deshkiupasana/settings.py
+++ deshkiupasana/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,16 +115,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 ################################
-STATICFILES_DIRS = ([
-    os.path.join(os.path.dirname(BASE_DIR), 'Static'),
-])
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Static'),
+]
 
 STATIC_URL = '/Static/'
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ######################
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'

ab1b1bdccf24bb54d0ecdde550083ed755a9de0f
abhishek <kumar008abhishek>
2020-06-07 14:16:55 +0530

    whitenoise

--- deshkiupasana/settings.py
+++ deshkiupasana/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from news.secret import secret
 SECRET_KEY = secret
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['deshkiupasana.herokuapp.com','127.0.0.1']
 
